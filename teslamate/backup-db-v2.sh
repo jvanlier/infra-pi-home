0012357a426906648eaf8d64bb49b08ea0c80f1a
Jori van Lier <pi-github@oci.one>
2022-02-02 14:39:56 +0100

    Add path to backup script

--- teslamate/backup-db-v2.sh
+++ teslamate/backup-db-v2.sh
@@ -3,6 +3,7 @@ set -eux
 
 DATE=$(date +%Y-%m-%d)
 FN=teslamate_dump_${DATE}.bck
+PATH=$PATH:/home/pi/.local/bin:/usr/local/bin:/bin
 
 docker-compose exec -T database pg_dump -U teslamate teslamate > ${FN}
 # Parallel bzip2:
