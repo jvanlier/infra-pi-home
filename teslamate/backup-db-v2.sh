bc5bb58f935daeba67d912ba8c3438c68359206c
Jori <git-public@jori.vanlier.io>
2024-11-02 20:42:52 +0100

    Add TTL to teslamate backup script

--- teslamate/backup-db-v2.sh
+++ teslamate/backup-db-v2.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -eux
 
+PATH=$PATH:/usr/local/bin:/bin
+
 DATE=$(date +%Y-%m-%d)
-FN=teslamate_dump_${DATE}.bck
-PATH=$PATH:/home/pi/.local/bin:/usr/local/bin:/bin
+DATE_DELETE=$(date -d '-3 months' +%Y-%m-%d)
+FN_PREFIX=teslamate_dump
+FN=${FN_PREFIX}_${DATE}.bck
+FN_DELETE=${FN_PREFIX}_${DATE_DELETE}.bck
 
 docker-compose exec -T database pg_dump -U teslamate teslamate > ${FN}
 # Parallel bzip2:
@@ -11,4 +15,5 @@ pbzip2 ${FN}
 
 aws s3 cp ${FN}.bz2 s3://jvl-bak
 rm ${FN}.bz2
+aws s3 rm s3://jvl-bak/${FN_DELETE}
 
